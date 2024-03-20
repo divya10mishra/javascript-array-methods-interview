f808c1fc9cf2edfddb52aa3d02a1f50ce9fd9beb
Divya Mishra <43552180+divya10mishra@users.noreply.github.com>
2024-03-20 14:48:08 +0530

    Update DuplicatesInArray.js

--- example-questions/DuplicatesInArray.js
+++ example-questions/DuplicatesInArray.js
@@ -5,13 +5,6 @@ const array = [20,30,30,40,50,60,60,60];
     return array.indexOf(ele)===index
   })
 console.log(aar3);
-
-//other method - reduce
-const uniqueElements = arr.reduce((acc,ele)=>{
-  acc[ele] = (acc[ele]||0)+1
-  return acc
-})
-
 //output - [20,30,40,50,60]
 
 //------------------------------------------------------------------------
@@ -24,9 +17,9 @@ const uniqueElements = arr.reduce((acc,ele)=>{
   acc[ele] = (acc[ele]||0)+1
   return acc
 },{})
-//console.log(uniqueElements,"uniqueElements")
-//output:{"3":2,"4":2,"6":1}
-//if initial is not set to object. output:3,4,6 [see above snippet]
-const noRepeat = Object.keys(uniqueElements).find(key => uniqueElements[key] === 1)
+//console.log(uniqueElements,"uniqueElements"). output:{"3":2,"4":2,"6":1}
 
-console.log(noRepeat)//output:6
+//if initial is not set to object. output:3(count of unique elements) 
+
+const noRepeat = Object.keys(uniqueElements).find(key => uniqueElements[key] === 1)
+console.log(noRepeat)//output:6 (elemts occured once)
